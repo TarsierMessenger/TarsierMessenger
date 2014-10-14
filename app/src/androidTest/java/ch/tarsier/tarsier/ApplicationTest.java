6a6ac88f816b894ea23ff42a11a81680af51d44c
Romain Ruetschi <romain.ruetschi@gmail.com>
2014-10-14 19:21:00 +0200

    Add dummy unit test.

--- app/src/androidTest/java/ch/tarsier/tarsier/ApplicationTest.java
+++ app/src/androidTest/java/ch/tarsier/tarsier/ApplicationTest.java
@@ -10,4 +10,10 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
     }
+
+    public void testDummy() throws Exception {
+        final int expected = 1;
+        final int reality = 1;
+        assertEquals(expected, reality);
+    }
 }
