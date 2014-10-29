59ed9c335befc0374a442c1583ffc0208e263d22
marinnicolini <marin-jerry.nicolini@epfl.ch>
2014-10-29 12:51:49 +0100

    In Action Bar -- Button to go backside

--- app/src/main/java/ch/tarsier/tarsier/PrivateDiscussionActivity.java
+++ app/src/main/java/ch/tarsier/tarsier/PrivateDiscussionActivity.java
@@ -1,8 +1,10 @@
 package ch.tarsier.tarsier;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 
@@ -12,14 +14,19 @@ public class PrivateDiscussionActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_private_discussion);
+
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.private_discussion, menu);
-        return true;
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.private_discussion, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
