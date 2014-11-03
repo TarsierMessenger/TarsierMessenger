bb6c5a9c872fdb82676c588359e1405114d2460f
Gabriel Luthier <gabluthier@gmail.com>
2014-11-03 11:18:51 +0100

    Remove support-annotations

---
+++ app/src/androidTest/java/ch/tarsier/tarsier/test/DiscussionsActivityTest.java
@@ -0,0 +1,35 @@
+package ch.tarsier.tarsier.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import ch.tarsier.tarsier.R;
+
+import ch.tarsier.tarsier.DiscussionsActivity;
+
+import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView;
+import static com.google.android.apps.common.testing.ui.espresso.Espresso.pressBack;
+import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.clearText;
+import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.click;
+import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.typeText;
+import static com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions.matches;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.isClickable;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withId;
+
+/**
+ * Created by gluthier on 31.10.2014.
+ */
+public class DiscussionsActivityTest extends ActivityInstrumentationTestCase2<DiscussionsActivity> {
+
+    public DiscussionsActivityTest() {
+        super(DiscussionsActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        getActivity();
+    }
+
+    public void testListDiscussionsShouldNotBeNull() {
+        onView(withId(R.id.list_discussions)).check(matches(isClickable()));
+    }
+}
