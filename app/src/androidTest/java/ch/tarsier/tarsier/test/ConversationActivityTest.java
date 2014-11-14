018f51d92050013a98c983c30a1dd24c085407e2
marinnicolini <marin-jerry.nicolini@epfl.ch>
2014-11-14 15:06:24 +0100

    testSendMessageButtonClickability

--- app/src/androidTest/java/ch/tarsier/tarsier/test/ConversationActivityTest.java
+++ app/src/androidTest/java/ch/tarsier/tarsier/test/ConversationActivityTest.java
@@ -1,19 +1,16 @@
 package ch.tarsier.tarsier.test;
 
 import android.test.ActivityInstrumentationTestCase2;
-
 import ch.tarsier.tarsier.R;
 import ch.tarsier.tarsier.ui.activity.ConversationActivity;
-
-import static com.google.android.apps.common.testing.ui.espresso.Espresso.onData;
 import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView;
 import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.clearText;
-import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.click;
 import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.closeSoftKeyboard;
 import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.typeText;
 import static com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions.matches;
 import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.isClickable;
 import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.not;
 
 /**
  * Created by Marin on 12.11.2014.
@@ -31,18 +28,21 @@ public class ConversationActivityTest extends ActivityInstrumentationTestCase2<C
         getActivity();
     }
 
+    /* test the sendImageButton if is clickable when something is to be sent or not*/
     public void testSendMessageButtonClickable() {
-        /*
-         Fix if sendImageButton should be always clickable or just when something is contained
-         in message_to_send
-         */
-        onView(withId(R.id.message_to_send)).perform(click());
-        //nothing should happen
-        onView(withId(R.id.message_to_send)).perform(typeText("This is a new message to be sent."));
+        String messageSent="This is a new message to be sent.";
+        onView(withId(R.id.sendImageButton)).check(matches(not(isClickable())));
+        onView(withId(R.id.message_to_send)).perform(typeText(messageSent));
         onView(withId(R.id.sendImageButton)).check(matches(isClickable()));
         onView(withId(R.id.message_to_send)).perform(clearText(), closeSoftKeyboard());
-        onView(withId(R.id.sendImageButton)).perform(click());
-        //nothing should happen
+        onView(withId(R.id.sendImageButton)).check(matches(not(isClickable())));
     }
 
+    /*
+    test if the scrolling fetch the last messages
+
+    public void testScrollToFetchMessages() {
+
+    }
+    */
 }
