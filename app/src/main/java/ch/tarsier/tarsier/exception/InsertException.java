8ad866fd2c3e028ce1bc2126526e84b520508a1b
Gabriel Luthier <gabluthier@gmail.com>
2014-11-16 21:07:08 +0100

    handle the errors

--- app/src/main/java/ch/tarsier/tarsier/exception/InsertException.java
+++ app/src/main/java/ch/tarsier/tarsier/exception/InsertException.java
@@ -3,5 +3,13 @@ package ch.tarsier.tarsier.exception;
 /**
  * @author gluthier
  */
-public class InsertException {
+public class InsertException extends Exception {
+
+    public InsertException() {
+        super();
+    }
+
+    public InsertException(String detailMessage) {
+        super(detailMessage);
+    }
 }
--- app/src/main/java/ch/tarsier/tarsier/exception/NoSuchModelException.java
+++ app/src/main/java/ch/tarsier/tarsier/exception/NoSuchModelException.java
@@ -3,5 +3,13 @@ package ch.tarsier.tarsier.exception;
 /**
  * @author gluthier
  */
-public class NoSuchModelException {
+public class NoSuchModelException extends Exception {
+
+    public NoSuchModelException() {
+        super();
+    }
+
+    public NoSuchModelException(String detailMessage) {
+        super(detailMessage);
+    }
 }
