92c06dd75d2b450745487faead16366490c2c28d
Gabriel Luthier <gabluthier@gmail.com>
2014-11-16 12:13:53 +0100

    generate the chats

--- app/src/main/java/ch/tarsier/tarsier/FillDatabaseWithFictionalData.java
+++ app/src/main/java/ch/tarsier/tarsier/FillDatabaseWithFictionalData.java
@@ -3,8 +3,6 @@ package ch.tarsier.tarsier;
 import java.util.Date;
 import java.util.Random;
 
-import ch.tarsier.tarsier.domain.model.User;
-import ch.tarsier.tarsier.domain.repository.UserRepository;
 import ch.tarsier.tarsier.storage.StorageAccess;
 
 /**
@@ -27,36 +25,56 @@ public class FillDatabaseWithFictionalData {
         Peer amirreza = new Peer("Amirreza Bahreini");
         Peer benjamin = new Peer("Benjamin Paccaud");
         Peer frederic = new Peer("Frederic Jacobs");
-        Peer gabriel = new Peer("Gabriel Luthier");
+        User gabriel = new User("Gabriel Luthier");
         Peer marin = new Peer("Marin-Jerry Nicolini");
         Peer romain = new Peer("Romain Ruetschi");
         Peer xavier = new Peer("Xavier Willemin");
         Peer yann = new Peer("Yann Mahmoudi");
 
-        final long amirrezaId = amirrezaP.getId();
-        final long benjaminId = benjaminP.getId();
-        final long fredericId = fredericP.getId();
-        final long marinId = marinP.getId();
-        final long romainId = romainP.getId();
-        final long xavierId = xavierP.getId();
-        final long yannId = yannP.getId();
+        final long amirrezaId = amirreza.getId();
+        final long benjaminId = benjamin.getId();
+        final long fredericId = frederic.getId();
+        final long gabrielId = gabriel.getId();
+        final long marinId = marin.getId();
+        final long romainId = romain.getId();
+        final long xavierId = xavier.getId();
+        final long yannId = yann.getId();
 
-        final String amirrezaName = amirrezaP.getName();
-        final String benjaminName = benjaminP.getName();
-        final String fredericName = fredericP.getName();
-        final String marinName = marinP.getName();
-        final String romainName = romainP.getName();
-        final String xavierName = xavierP.getName();
-        final String yannName = yannP.getName();
+        final String amirrezaName = amirreza.getName();
+        final String benjaminName = benjamin.getName();
+        final String fredericName = frederic.getName();
+        final String gabrielName = gabriel.getName();
+        final String marinName = marin.getName();
+        final String romainName = romain.getName();
+        final String xavierName = xavier.getName();
+        final String yannName = yann.getName();
 
-        peerRepository.insert(amirrezaP);
-        peerRepository.insert(benjaminP);
-        peerRepository.insert(fredericP);
-        peerRepository.insert(gabrielP);
-        peerRepository.insert(marinP);
-        peerRepository.insert(romainP);
-        peerRepository.insert(xavierP);
-        peerRepository.insert(yannP);
+        peerRepository.insert(amirreza);
+        peerRepository.insert(benjamin);
+        peerRepository.insert(frederic);
+        peerRepository.insert(gabriel);
+        peerRepository.insert(marin);
+        peerRepository.insert(romain);
+        peerRepository.insert(xavier);
+        peerRepository.insert(yann);
+
+        //Generate the chats
+        ArrayList<Chat> chatList = new ArrayList<Chat>(10);
+
+        chatList.add(new Chat(frederic));
+        chatList.add(new Chat(marin));
+        chatList.add(new Chat("SwEng", frederic));
+        chatList.add(new Chat(romain));
+        chatList.add(new Chat(amirreza));
+        chatList.add(new Chat(xavier));
+        chatList.add(new Chat(yann));
+        chatList.add(new Chat("Git helpdesk", romain));
+        chatList.add(new Chat(benjamin));
+        chatList.add(new Chat("Saaaaat", gabriel));
+
+        for (Chat c : chatList) {
+            chatRepository.insert(c);
+        }
 
          */
 
