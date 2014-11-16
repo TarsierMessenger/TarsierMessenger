efe8a07f91c757fcdcf032d710126a9fb457dc25
marinnicolini <marin-jerry.nicolini@epfl.ch>
2014-11-16 16:44:55 +0100

    Unit Test testGetTimestamp
    Conflicts:
    	app/src/androidTest/java/ch/tarsier/tarsier/test/ConversationActivityTest.java

---
+++ app/src/androidTest/java/ch/tarsier/tarsier/test/DateUtilTest.java
@@ -0,0 +1,44 @@
+package ch.tarsier.tarsier.test;
+
+import android.test.AndroidTestCase;
+
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import ch.tarsier.tarsier.util.DateUtil;
+
+/**
+ * Created by Marin on 16.11.2014.
+ */
+public class DateUtilTest extends AndroidTestCase {
+    public DateUtilTest() {
+        super();
+    }
+    public void testGetTimestamp(){
+        //Set the time to now
+        long timeStamp = DateUtil.getNowTimestamp();
+        String dateSeparator = DateUtil.computeDateSeparator(timeStamp);
+        assertEquals("Today", dateSeparator);
+
+        // Set the time to yesterday
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_MONTH, -1);
+        dateSeparator = DateUtil.computeDateSeparator(calendar.getTimeInMillis());
+        assertEquals("Yesterday", dateSeparator);
+
+        //Set time to the day before yesterday
+        calendar.add(Calendar.DAY_OF_MONTH,-1);
+        SimpleDateFormat format = new SimpleDateFormat("E");
+        dateSeparator = DateUtil.computeDateSeparator(calendar.getTimeInMillis());
+        assertEquals(format.format(calendar.getTimeInMillis()), dateSeparator);
+
+        // Set time to the a day more than one week ago
+        calendar.add(Calendar.DAY_OF_MONTH,-9);
+        dateSeparator = DateUtil.computeDateSeparator(calendar.getTimeInMillis());
+        format = new SimpleDateFormat("dd.MM.yyyy");
+        assertEquals(format.format(calendar.getTimeInMillis()), dateSeparator);
+    }
+}
