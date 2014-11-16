5e9bb5ffec7699425afc1a898f07e390e0a05ffc
Gabriel Luthier <gabluthier@gmail.com>
2014-11-16 17:39:11 +0100

    create MessageRepository

---
+++ app/src/main/java/ch/tarsier/tarsier/domain/repository/MessageRepository.java
@@ -0,0 +1,15 @@
+package ch.tarsier.tarsier.domain.repository;
+
+import ch.tarsier.tarsier.database.Database;
+
+/**
+ * @author gluthier
+ */
+public class MessageRepository extends AbstractRepository {
+
+    public MessageRepository(Database database) {
+        super(database);
+    }
+
+
+}
