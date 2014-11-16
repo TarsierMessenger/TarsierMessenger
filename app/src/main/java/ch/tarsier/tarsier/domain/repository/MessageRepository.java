e517efbaa4173cf10aabdc0f1608201a6c291212
Gabriel Luthier <gabluthier@gmail.com>
2014-11-16 18:06:23 +0100

    add empty MessageRepository

--- app/src/main/java/ch/tarsier/tarsier/domain/repository/MessageRepository.java
+++ app/src/main/java/ch/tarsier/tarsier/domain/repository/MessageRepository.java
@@ -1,6 +1,11 @@
 package ch.tarsier.tarsier.domain.repository;
 
+import android.database.Cursor;
+
+import ch.tarsier.tarsier.Tarsier;
+import ch.tarsier.tarsier.database.Columns;
 import ch.tarsier.tarsier.database.Database;
+import ch.tarsier.tarsier.domain.model.Message;
 
 /**
  * @author gluthier
@@ -11,5 +16,39 @@ public class MessageRepository extends AbstractRepository {
         super(database);
     }
 
-    
+    public Message findById(long id) {
+        //TODO
+        return null;
+    }
+
+    public void insert(Message message) {
+        //TODO
+    }
+
+    public void update(Message message) {
+        //TODO
+    }
+
+    public void delete(Message message) {
+        //TODO
+    }
+
+
+    private Message buildFromCursor(Cursor c) {
+        int chatId = c.getInt(c.getColumnIndex(Columns.Message.COLUMN_NAME_CHAT_ID));
+        String text = c.getString(c.getColumnIndex(Columns.Message.COLUMN_NAME_MSG));
+        long senderId = c.getLong(c.getColumnIndex(Columns.Message.COLUMN_NAME_SENDER_ID));
+        long dateTime = c.getLong(c.getColumnIndex(Columns.Message.COLUMN_NAME_DATETIME));
+
+        long userId = Tarsier.app().getUserPreferences().getId();
+
+        Message message = null;
+        if (senderId == userId) {
+            message = new Message(chatId, text, dateTime);
+        } else {
+            message = new Message(chatId, text, senderId, dateTime);
+        }
+
+        return message;
+    }
 }
