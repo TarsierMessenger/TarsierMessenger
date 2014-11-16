8f4caf9407f0a50c24995d2fe5b04224d1ae789f
Gabriel Luthier <gabluthier@gmail.com>
2014-11-16 21:07:08 +0100

    handle the errors

--- app/src/main/java/ch/tarsier/tarsier/exception/InvalidCursorException.java
+++ app/src/main/java/ch/tarsier/tarsier/exception/InvalidCursorException.java
@@ -4,4 +4,12 @@ package ch.tarsier.tarsier.exception;
  * @author gluthier
  */
 public class InvalidCursorException extends Exception {
+
+    public InvalidCursorException() {
+        super();
+    }
+
+    public InvalidCursorException(String detailMessage) {
+        super(detailMessage);
+    }
 }
