8ca864259618fe3b5869b1bed3d86398d59d1453
Romain Ruetschi <romain.ruetschi@gmail.com>
2014-11-17 22:32:03 +0100

    Fix some Checkstyle warnings in 'network.old' package.

--- app/src/main/java/ch/tarsier/tarsier/network/old/ChatRoom.java
+++ app/src/main/java/ch/tarsier/tarsier/network/old/ChatRoom.java
@@ -19,85 +19,85 @@ import ch.tarsier.tarsier.R;
 /**
  * Created by amirreza on 10/27/14.
  */
-    public class ChatRoom extends Fragment {
+public class Chatroom extends Fragment {
 
 
-        private MyConnection myConnection;
+    private MyConnection myConnection;
 
-        private View view;
-        private TextView chatLine;
-        private ListView listView;
-        ChatMessageAdapter adapter = null;
-        private List<String> items = new ArrayList<String>();
+    private View view;
+    private TextView chatLine;
+    private ListView listView;
+    ChatMessageAdapter adapter = null;
+    private List<String> items = new ArrayList<String>();
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            view = inflater.inflate(R.layout.chat_room, container, false);
-            chatLine = (TextView) view.findViewById(R.id.txtChatLine);
-            listView = (ListView) view.findViewById(android.R.id.list);
-            adapter = new ChatMessageAdapter(getActivity(), android.R.id.text1,
-                    items);
-            listView.setAdapter(adapter);
-            view.findViewById(R.id.button1).setOnClickListener(
-                    new View.OnClickListener() {
-                        @Override
-                        public void onClick(View arg0) {
-                            if (myConnection != null) {
-                                myConnection.write(chatLine.getText().toString()
-                                        .getBytes());
-                                pushMessage("Me: " + chatLine.getText().toString());
-                                chatLine.setText("");
-                                chatLine.clearFocus();
-                            }
-                        }
-                    });
-            return view;
-        }
-        public interface MessageTarget {
-            public Handler getHandler();
-        }
-        public void setMyConnection(MyConnection obj) {
-            myConnection = obj;
-        }
-        public void pushMessage(String readMessage) {
-            adapter.add(readMessage);
-            adapter.notifyDataSetChanged();
-        }
-        /**
-         * ArrayAdapter to manage chat messages.
-         */
-        public class ChatMessageAdapter extends ArrayAdapter<String> {
-            List<String> messages = null;
-            public ChatMessageAdapter(Context context, int textViewResourceId,
-                                      List<String> items) {
-                super(context, textViewResourceId, items);
-            }
-            @Override
-            public View getView(int position, View convertView, ViewGroup parent) {
-                View v = convertView;
-                if (v == null) {
-                    LayoutInflater vi = (LayoutInflater) getActivity()
-                            .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                    v = vi.inflate(android.R.layout.simple_list_item_1, null);
-                }
-                String message = items.get(position);
-                if (message != null && !message.isEmpty()) {
-                    TextView nameText = (TextView) v
-                            .findViewById(android.R.id.text1);
-                    if (nameText != null) {
-                        nameText.setText(message);
-                        if (message.startsWith("Me: ")) {
-                            nameText.setTextAppearance(getActivity(),
-                                    R.style.normalText);
-                        } else {
-                            nameText.setTextAppearance(getActivity(),
-                                    R.style.boldText);
-                        }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.chat_room, container, false);
+        chatLine = (TextView) view.findViewById(R.id.txtChatLine);
+        listView = (ListView) view.findViewById(android.R.id.list);
+        adapter = new ChatMessageAdapter(getActivity(), android.R.id.text1,
+                items);
+        listView.setAdapter(adapter);
+        view.findViewById(R.id.button1).setOnClickListener(
+            new View.OnClickListener() {
+                @Override
+                public void onClick(View arg0) {
+                    if (myConnection != null) {
+                        myConnection.write(chatLine.getText().toString()
+                                .getBytes());
+                        pushMessage("Me: " + chatLine.getText().toString());
+                        chatLine.setText("");
+                        chatLine.clearFocus();
                     }
                 }
-                return v;
-            }
+            });
+        return view;
+    }
+    public interface MessageTarget {
+        public Handler getHandler();
+    }
+    public void setMyConnection(MyConnection obj) {
+        myConnection = obj;
+    }
+    public void pushMessage(String readMessage) {
+        adapter.add(readMessage);
+        adapter.notifyDataSetChanged();
+    }
+    /**
+     * ArrayAdapter to manage chat messages.
+     */
+    public class ChatMessageAdapter extends ArrayAdapter<String> {
+        List<String> messages = null;
+        public ChatMessageAdapter(Context context, int textViewResourceId,
+                                  List<String> items) {
+            super(context, textViewResourceId, items);
         }
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View v = convertView;
+            if (v == null) {
+                LayoutInflater vi = (LayoutInflater) getActivity()
+                        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                v = vi.inflate(android.R.layout.simple_list_item_1, null);
+            }
+            String message = items.get(position);
+            if (message != null && !message.isEmpty()) {
+                TextView nameText = (TextView) v
+                        .findViewById(android.R.id.text1);
+                if (nameText != null) {
+                    nameText.setText(message);
+                    if (message.startsWith("Me: ")) {
+                        nameText.setTextAppearance(getActivity(),
+                                R.style.normalText);
+                    } else {
+                        nameText.setTextAppearance(getActivity(),
+                                R.style.boldText);
+                    }
+                }
+            }
+            return v;
+        }
+    }
 
 }
