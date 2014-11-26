8e35dbd71876a4d33086f32163ce13c41dd45ea8
Benjamin Paccaud <benjamin.paccaud@epfl.ch>
2014-11-26 10:49:45 +0100

    NearbyPeer tab shows a dummy list of PeerViewModel

--- app/src/main/java/ch/tarsier/tarsier/ui/activity/NearbyPeerFragment.java
+++ app/src/main/java/ch/tarsier/tarsier/ui/activity/NearbyPeerFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import android.widget.ListView;
 
 import ch.tarsier.tarsier.R;
