83ec89305600177cbf19832c5f6fe51e563b9524
Benjamin Paccaud <benjamin.paccaud@epfl.ch>
2014-11-28 17:08:25 +0100

    Use the eventBus to set the peer list

--- app/src/main/java/ch/tarsier/tarsier/ui/fragment/NearbyPeerFragment.java
+++ app/src/main/java/ch/tarsier/tarsier/ui/fragment/NearbyPeerFragment.java
@@ -3,16 +3,22 @@ package ch.tarsier.tarsier.ui.fragment;
 import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
+import com.squareup.otto.Subscribe;
+
 import java.util.ArrayList;
 import java.util.List;
 
 import ch.tarsier.tarsier.R;
+import ch.tarsier.tarsier.Tarsier;
 import ch.tarsier.tarsier.domain.model.Peer;
+import ch.tarsier.tarsier.event.ReceivedChatroomPeersListEvent;
+import ch.tarsier.tarsier.event.ReceivedNearbyPeersListEvent;
 import ch.tarsier.tarsier.ui.adapter.PeerAdapter;
 
 /**
@@ -22,22 +28,30 @@ public class NearbyPeerFragment extends Fragment {
 
     private Activity mActivity;
     private PeerAdapter mPeerAdapter;
+    private int count =0;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Tarsier.app().getEventBus().register(this);
+        mPeerAdapter = new PeerAdapter(mActivity,R.layout.row_nearby_peer_list);
+        //debug
     }
 
-    private void updatePeerAdapter() {
-        mPeerAdapter = new PeerAdapter(mActivity,R.layout.row_nearby_peer_list);
-        mPeerAdapter.setPeerList(getListPeers());
+    @Subscribe
+    public void receivedNewPeersList(ReceivedNearbyPeersListEvent event) {
+        mPeerAdapter.clear();
+        mPeerAdapter.setPeerList(event.getPeers());
+        Log.d("EventRecieved", "The event has been recieved");
     }
 
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
         mActivity = activity;
-        updatePeerAdapter();
+        //updatePeerAdapter();
+        //List<Peer> peerList = getListPeers();
+        //Tarsier.app().getEventBus().post(new ReceivedNearbyPeersListEvent(peerList));
     }
 
     @Override
@@ -51,11 +65,15 @@ public class NearbyPeerFragment extends Fragment {
     private List<Peer> getListPeers() {
         //TODO get the peers in the right way
         List<Peer> peers = new ArrayList<Peer>();
-        peers.add(new Peer("Ben","J'aime les cacahuetes"));
-        peers.add(new Peer("gluthier","J'aime les fraises"));
-        peers.add(new Peer("amirezza","J'aime les patates"));
-        peers.add(new Peer("Marin","J'aime les petits pois"));
-        peers.add(new Peer("Fred","J'aime les pommes"));
+        peers.add(new Peer("Ben", "J'aime les cacahuetes"));
+        peers.add(new Peer("gluthier", "J'aime les fraises"));
+        peers.add(new Peer("amirezza", "J'aime les patates"));
+        peers.add(new Peer("Marin", "J'aime les petits pois"));
+        peers.add(new Peer("Fred", "J'aime les pommes"));
+
+//            peers.add(new Peer("Benpac","I am immortal"));
+//            peers.add(new Peer("Romac", "Shut up ben"));
+
         return peers;
     }
 }
