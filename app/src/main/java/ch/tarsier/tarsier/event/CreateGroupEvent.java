29b14ad88af2593d9f289914243a5b18e15f9405
Romain Ruetschi <romain.ruetschi@gmail.com>
2014-11-30 18:53:15 +0100

    Store the Chat inside of CreateGroupEvent.

--- app/src/main/java/ch/tarsier/tarsier/event/CreateGroupEvent.java
+++ app/src/main/java/ch/tarsier/tarsier/event/CreateGroupEvent.java
@@ -1,9 +1,20 @@
 package ch.tarsier.tarsier.event;
 
+import ch.tarsier.tarsier.domain.model.Chat;
+
 /**
  * @author romac
  * @author amirezza
  */
 public class CreateGroupEvent {
 
+    private final Chat mChat;
+
+    public CreateGroupEvent(Chat chat) {
+        mChat = chat;
+    }
+
+    public Chat getChat() {
+        return mChat;
+    }
 }
