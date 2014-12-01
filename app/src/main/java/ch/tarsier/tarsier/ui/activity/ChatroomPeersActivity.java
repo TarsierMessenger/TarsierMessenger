543346980df6f0bc1a9dee670aa616bd86180a2c
Romain Ruetschi <romain.ruetschi@gmail.com>
2014-12-01 14:21:04 +0100

    Enable back button in ChatroomPeersActivity.

--- app/src/main/java/ch/tarsier/tarsier/ui/activity/ChatroomPeersActivity.java
+++ app/src/main/java/ch/tarsier/tarsier/ui/activity/ChatroomPeersActivity.java
@@ -39,6 +39,10 @@ public class ChatroomPeersActivity extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        if (getActionBar() != null) {
+            getActionBar().setHomeButtonEnabled(true);
+        }
+
         setUpData();
         setUpEvent();
 
@@ -119,14 +123,15 @@ public class ChatroomPeersActivity extends ListActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id) {
+            case android.R.id.home:
+                finish();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
         }
-        return super.onOptionsItemSelected(item);
     }
 
     /**
