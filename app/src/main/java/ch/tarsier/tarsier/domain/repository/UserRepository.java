14e287636f133899918b3a37241980f7175eada9
Romain Ruetschi <romain.ruetschi@gmail.com>
2014-12-01 22:14:25 +0100

    Set the user's public key on the User model.

--- app/src/main/java/ch/tarsier/tarsier/domain/repository/UserRepository.java
+++ app/src/main/java/ch/tarsier/tarsier/domain/repository/UserRepository.java
@@ -2,6 +2,7 @@ package ch.tarsier.tarsier.domain.repository;
 
 import ch.tarsier.tarsier.Tarsier;
 import ch.tarsier.tarsier.domain.model.User;
+import ch.tarsier.tarsier.domain.model.value.PublicKey;
 import ch.tarsier.tarsier.prefs.UserPreferences;
 
 /**
@@ -20,11 +21,11 @@ public class UserRepository {
     public User getUser() {
         if (mUser == null) {
             mUser = new User();
-
             mUser.setUserName(mUserPreferences.getUsername());
             mUser.setStatusMessage(mUserPreferences.getStatusMessage());
             mUser.setPicturePath(mUserPreferences.getPicturePath());
             mUser.setOnline(true);
+            mUser.setPublicKey(new PublicKey(mUserPreferences.getKeyPair().getPublicKey()));
         }
 
         return mUser;
