704037494ca0746491aedfddff3f3074da1b7757
Romain Ruetschi <romain.ruetschi@gmail.com>
2014-12-02 09:50:59 +0100

    Start on NearbyActivity.

--- app/src/main/java/ch/tarsier/tarsier/ui/activity/HomeActivity.java
+++ app/src/main/java/ch/tarsier/tarsier/ui/activity/HomeActivity.java
@@ -55,9 +55,9 @@ public class HomeActivity extends Activity {
         if (!mUserPreferences.getUsername().equals("")
             && !mUserPreferences.getStatusMessage().equals("")) {
 
-            Intent chatListIntent = new Intent(this, ChatListActivity.class);
-            startActivity(chatListIntent);
-            this.finish();
+            Intent nearbyListActivity = new Intent(this, NearbyListActivity.class);
+            startActivity(nearbyListActivity);
+            finish();
         }
 
         refreshFields();
