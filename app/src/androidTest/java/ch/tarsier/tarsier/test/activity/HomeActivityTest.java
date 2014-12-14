655ad2304d62ceae1d509d07749c1114ab3b936e
Benjamin Paccaud <benjamin.paccaud@epfl.ch>
2014-12-14 17:12:05 +0100

    Update HomeActivityTest

--- app/src/androidTest/java/ch/tarsier/tarsier/test/activity/HomeActivityTest.java
+++ app/src/androidTest/java/ch/tarsier/tarsier/test/activity/HomeActivityTest.java
@@ -1,7 +1,23 @@
 package ch.tarsier.tarsier.test.activity;
 
+import ch.tarsier.tarsier.R;
+import ch.tarsier.tarsier.Tarsier;
+import ch.tarsier.tarsier.prefs.UserPreferences;
 import ch.tarsier.tarsier.test.TarsierTestCase;
 import ch.tarsier.tarsier.ui.activity.HomeActivity;
+
+import static ch.tarsier.tarsier.test.matchers.HasErrorMatcher.hasError;
+import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView;
+import static com.google.android.apps.common.testing.ui.espresso.Espresso.pressBack;
+import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.clearText;
+import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.click;
+import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.closeSoftKeyboard;
+import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.typeText;
+import static com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions.matches;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.isClickable;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withId;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * HomeActivityTest test the HomeActivity class.
@@ -15,7 +31,7 @@ public class HomeActivityTest extends TarsierTestCase<HomeActivity> {
         super(HomeActivity.class);
     }
 
-    /*
+
     private static final String STATUS_TOO_LONG = "A very long status message that should not pass "
                                                    + "as it clearly has more than 50 characters";
     private static final String WHITE_SPACE = "  \n ";
@@ -25,10 +41,12 @@ public class HomeActivityTest extends TarsierTestCase<HomeActivity> {
     private static final String EMPTY_STRING = "";
 
     private UserPreferences mUserPrefMock;
+    private UserPreferences mUserPrefStore;
 
     @Override
     protected void setUp() throws Exception {
         super.setUp();
+        mUserPrefStore = Tarsier.app().getUserPreferences();
         mUserPrefMock = mock(UserPreferences.class);
         when(mUserPrefMock.getStatusMessage()).thenReturn(EMPTY_STRING);
         when(mUserPrefMock.getUsername()).thenReturn(EMPTY_STRING);
@@ -37,38 +55,37 @@ public class HomeActivityTest extends TarsierTestCase<HomeActivity> {
         getActivity();
     }
 
+    /**
+     * put back the original UserPreference when the test is done
+     * @throws Exception
+     */
+    @Override
+    protected void tearDown() throws Exception {
+        Tarsier.app().setUserPreferences(mUserPrefStore);
+        super.tearDown();
+    }
+
     public void testStartButtonClickability() {
         emptyUsernameAndStatus();
 
-        onView(withId(R.id.lets_chat)).check(matches(not(isClickable())));
+        onView(withId(R.id.lets_chat)).perform(click());
+
+        onView(withId(R.id.username_home)).check(matches(hasError(R.string.error_username_length)));
+        onView(withId(R.id.status_message_home)).check(matches(hasError(R.string.error_status_message_length)));
 
         onView(withId(R.id.username_home)).perform(typeText(USERNAME_OK), closeSoftKeyboard());
-        onView(withId(R.id.lets_chat)).check(matches(not(isClickable())));
-
         onView(withId(R.id.status_message_home)).perform(typeText(STATUS_OK), closeSoftKeyboard());
-        onView(withId(R.id.lets_chat)).check(matches(isClickable()));
 
-        onView(withId(R.id.username_home)).perform(clearText(), closeSoftKeyboard());
-        onView(withId(R.id.lets_chat)).check(matches(not(isClickable())));
+        onView(withId(R.id.lets_chat)).perform(click());
 
-        onView(withId(R.id.username_home)).perform(typeText(USERNAME_OK), closeSoftKeyboard());
-        onView(withId(R.id.lets_chat)).check(matches(isClickable()));
 
-        onView(withId(R.id.status_message_home)).perform(clearText(),
-                                                    typeText(STATUS_TOO_LONG),
-                                                    closeSoftKeyboard());
-        onView(withId(R.id.lets_chat)).check(matches(not(isClickable())));
-
-        onView(withId(R.id.status_message_home)).perform(clearText(), typeText(STATUS_OK), closeSoftKeyboard());
-        onView(withId(R.id.username_home)).perform(clearText(), typeText(USERNAME_TOO_LONG), closeSoftKeyboard());
-        onView(withId(R.id.lets_chat)).check(matches(not(isClickable())));
     }
 
     public void testAddPictureClick() {
         onView(withId(R.id.picture)).check(matches(isClickable()));
         onView(withId(R.id.picture)).perform(click());
 
-        we are on AddProfilePictureActivity
+        //we are on AddProfilePictureActivity
         onView(withId(R.id.add_from_existing)).check(matches(isClickable()));
         onView(withId(R.id.add_new_picture)).check(matches(isClickable()));
 
@@ -76,21 +93,31 @@ public class HomeActivityTest extends TarsierTestCase<HomeActivity> {
         pressBack();
     }
 
+    public void clickLetsChat() {
+        onView(withId(R.id.lets_chat)).perform(click());
+    }
+
     public void testUsernameTooShort() {
 
         emptyUsernameAndStatus();
         onView(withId(R.id.username_home)).perform(typeText(USERNAME_OK), closeSoftKeyboard());
         onView(withId(R.id.username_home)).perform(clearText());
+        clickLetsChat();
         onView(withId(R.id.username_home)).check(matches(hasError(R.string.error_username_length)));
         onView(withId(R.id.username_home)).perform(clearText(), typeText(WHITE_SPACE), closeSoftKeyboard());
+        clickLetsChat();
         onView(withId(R.id.username_home)).check(matches(hasError(R.string.error_username_whitespace)));
+
+
     }
+
 
     public void testUsernameTooLong() {
         emptyUsernameAndStatus();
         onView(withId(R.id.username_home)).perform(
                 typeText(USERNAME_TOO_LONG),
                 closeSoftKeyboard());
+        clickLetsChat();
         onView(withId(R.id.username_home)).check(matches(hasError(R.string.error_username_length)));
     }
 
@@ -98,8 +125,10 @@ public class HomeActivityTest extends TarsierTestCase<HomeActivity> {
         emptyUsernameAndStatus();
         onView(withId(R.id.status_message_home)).perform(typeText(USERNAME_OK), closeSoftKeyboard());
         onView(withId(R.id.status_message_home)).perform(clearText());
+        clickLetsChat();
         onView(withId(R.id.status_message_home)).check(matches(hasError(R.string.error_status_message_length)));
         onView(withId(R.id.status_message_home)).perform(clearText(), typeText(WHITE_SPACE), closeSoftKeyboard());
+        clickLetsChat();
         onView(withId(R.id.status_message_home)).check(matches(hasError(R.string.error_status_message_whitespace)));
     }
 
@@ -108,6 +137,7 @@ public class HomeActivityTest extends TarsierTestCase<HomeActivity> {
                 clearText(),
                 typeText(STATUS_TOO_LONG),
                 closeSoftKeyboard());
+        clickLetsChat();
         onView(withId(R.id.status_message_home)).check(matches(hasError(R.string.error_status_message_length)));
     }
 
@@ -115,5 +145,5 @@ public class HomeActivityTest extends TarsierTestCase<HomeActivity> {
         onView(withId(R.id.status_message_home)).perform(clearText());
         onView(withId(R.id.username_home)).perform(clearText());
     }
-    */
+
 }
